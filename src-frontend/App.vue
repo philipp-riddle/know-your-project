694e79c2aafc4f852d54e48288a55391ec9ffcb2
philipp-riddle <philipp@riddle.com>
2025-03-01 23:53:10 +0100

    add: open search with command/shift/control F or P

--- src-frontend/App.vue
+++ src-frontend/App.vue
@@ -16,16 +16,34 @@
 <script setup>
     import { computed, onMounted } from 'vue';
     import { useRoute, useRouter } from 'vue-router';
+    import { useMagicKeys } from '@vueuse/core';
     import Navigation from '@/components/Navigation/Navigation.vue';
     import SearchModal from '@/components/Search/SearchModal.vue';
     import ThreadBox from '@/components/Thread/ThreadBox.vue';
     import { usePageStore } from '@/stores/PageStore.js';
     import { useProjectStore } from '@/stores/ProjectStore.js';
+    import { useSearchStore } from '@/stores/SearchStore.js';
 
     const currentRoute = useRoute();
     const router = useRouter();
     const pageStore = usePageStore();
     const projectStore = useProjectStore();
+    const searchStore = useSearchStore();
+
+    // specify the keys we want to monitor
+    const { control, shift, meta, p, f } = useMagicKeys({
+        passive: false,
+        onEventFired(e) {
+            const mainKeyIsPressed = control.value || shift.value || meta.value;
+            const secondKeyIsPressed = p.value || f.value;
+
+            if (mainKeyIsPressed && secondKeyIsPressed) {
+                e.preventDefault();
+                e.stopPropagation();
+                searchStore.toggleIsSearching();
+            }
+        }
+    });
 
     onMounted(() => {
         if (isUserInSetup.value && currentRoute.name !== 'Setup') {
